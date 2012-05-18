3243b53bea63d24553a4f5f85564635260980afe
Michael Mulley <michael@michaelmulley.com>
2012-05-18 14:37:56 -0400

    Enable debug server for static files

--- open511_site/urls.py
+++ open511_site/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib.gis import admin
 admin.autodiscover()
@@ -7,3 +8,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('open511.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns
