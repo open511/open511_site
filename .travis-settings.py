5867d1d05ac4e75d10219906e5aa0c5bf28dc182
Michael Mulley <michael@michaelmulley.com>
2013-08-01 01:02:53 -0400

    First draft of Travis config

---
+++ .travis-settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'open511',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+OPEN511_BASE_URL = 'http://open511.example.com'
+OPEN511_UI_TEST_BROWSER = 'phantomjs'
